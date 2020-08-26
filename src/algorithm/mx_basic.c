8211d3722fddd15d079b53f29b8e9d9d17c24ad3
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-08-26 21:17:50 +0300

    Fix output

--- src/algorithm/mx_basic.c
+++ src/algorithm/mx_basic.c
@@ -4,12 +4,15 @@
 
 #include <uls.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void print_dirent_arr(struct dirent **arr, char *delim) {
     for (int i = 0; arr[i]; i++) {
         mx_printstr(arr[i]->d_name);
-        mx_printstr(delim);
+        if (arr[i + 1] != 0)
+            mx_printstr(delim);
     }
+    mx_printstr("\n");
 }
 
 static void delete_dirent_arr(struct dirent ***arr) {
@@ -21,7 +24,8 @@ void mx_basic(const char *dir_name) {
     struct dirent **arr = 0;
 
     mx_scandir(dir_name, &arr, mx_select_exclude_dot_dirs, mx_alphasort);
-    print_dirent_arr(arr, "\n");
+    char *delim = isatty(STDOUT_FILENO) ? "\t\t" : "\n";
+    print_dirent_arr(arr, delim);
 
     delete_dirent_arr(&arr);
 }
