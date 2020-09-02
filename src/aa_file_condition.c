c2535e2a9cc4d4eebe756944aba3bf63467255d9
Karyna Barinova <kbarinova@e2r1p5.unit.ua>
2020-09-02 16:18:47 +0300

    removed duplicate file

--- src/aa_file_condition.c
+++
@@ -1,9 +0,0 @@
-#include <uls.h>
-
-bool mx_A_file_condition(char *name) {
-    if (mx_strcmp(".\0", name) != 0
-        && mx_strcmp("..\0", name) != 0) {
-        return true;
-    }
-    return false;
-}
