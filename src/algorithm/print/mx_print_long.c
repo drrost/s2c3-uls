1e2e53528cb563ea53527362e3b8aafbbaa2e620
Karyna Barinova <kbarinova@e3r5p1.unit.ua>
2020-09-10 11:52:09 +0300

    added time to -l

--- src/algorithm/print/mx_print_long.c
+++ src/algorithm/print/mx_print_long.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <grp.h>
+#include <time.h>
 
 void mx_print_permissions(mode_t mode) {
 	char perms[11];
@@ -77,6 +78,60 @@ char *mx_get_group(gid_t st_gid) {
 	return group;
 }
 
+static char *date_less(char *buf) {
+	char *res = mx_strnew(12);
+
+	res[0] = buf[4];
+	res[1] = buf[5];
+	res[2] = buf[6];
+	res[3] = ' ';
+	res[4] = buf[8];
+	res[5] = buf[9];
+	res[6] = ' ';
+	res[7] = buf[11];
+	res[8] = buf[12];
+	res[9] = buf[13];
+	res[10] = buf[14];
+	res[11] = buf[15];
+	return res;
+}
+
+static char *date_more(char *buf) {
+	char *res = mx_strnew(12);
+
+	res[0] = buf[4];
+	res[1] = buf[5];
+	res[2] = buf[6];
+	res[3] = ' ';
+	res[4] = buf[8];
+	res[5] = buf[9];
+	res[6] = ' ';
+	res[7] = ' ';
+	res[8] = buf[20];
+	res[9] = buf[21];
+	res[10] = buf[22];
+	res[11] = buf[23];
+	return res;
+}
+
+char *mx_get_time(struct stat buf) {
+	time_t time_f;
+	char *buff = NULL;
+	char *res = NULL;
+	time_t current = time(NULL);
+	time_t sec_in_six_month = 15778368;
+
+	time_f = buf.st_mtimespec.tv_sec;
+	buff = mx_strdup(ctime(&time_f));
+
+	if ((current - sec_in_six_month) > time_f || (current + sec_in_six_month) < time_f)
+		res = date_more(buff);
+	else
+		res = date_less(buff);
+	mx_strdel(&buff);
+	return res;
+}
+
 
 void mx_print_dir_long_format(t_list *entities, const char *delim) {
 	struct stat buf;
@@ -89,9 +144,13 @@ void mx_print_dir_long_format(t_list *entities, const char *delim) {
 		lstat(mx_find_index(entities, i), &buf);
 		mx_print_permissions(buf.st_mode);
 		mx_printstr(delim);
+		//mx_printstr(mx_get_size(buf));
+		//mx_printstr(delim);
 		mx_printstr(mx_get_owner(buf.st_uid));
 		mx_printstr(delim);
 		mx_printstr(mx_get_group(buf.st_gid));
+		mx_printstr(delim);
+		mx_printstr(mx_get_time(buf));
 		mx_printstr(delim);
 		mx_printstr(mx_find_index(entities, i));
 		mx_printstr("\n");
