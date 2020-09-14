6b1dd093f03ce5d1dee60e23dc016fd9b2729a49
Karyna Barinova <kbarinova@e3r6p2.unit.ua>
2020-09-14 17:43:37 +0300

    disabled flags check

--- src/algorithm/manage/mx_parse_arguments.c
+++ src/algorithm/manage/mx_parse_arguments.c
@@ -31,9 +31,6 @@ t_algorithm *mx_parse_arguments(int argc, char *argv[]) {
     int path_idx = argc > 1 && argv[1][0] == '-' ? 2 : 1;
     const char *line = argc > 1 && argv[1][0] == '-' ? argv[1] : "";
 
-    if (!check_valid_flags(line))
-        exit(1);
-
     t_flags flags = mx_parse_flags(line);
 
     mx_assign_fetchers(algorithm, &flags);
