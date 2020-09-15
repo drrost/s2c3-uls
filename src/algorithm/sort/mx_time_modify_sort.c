f635913e3397f709a5ba37bc9e3e0384a1346499
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-15 22:43:38 +0300

    Add -t flag

--- src/algorithm/sort/mx_time_modify_sort.c
+++ src/algorithm/sort/mx_time_modify_sort.c
@@ -5,6 +5,11 @@
 #include <libmx.h>
 
 int mx_time_modify_sort(t_dirent *d1, t_dirent *d2) {
-    return d1->file_stat.st_mtimespec.tv_nsec <
-           d2->file_stat.st_mtimespec.tv_nsec;
+    if (d1->file_stat.st_mtimespec.tv_sec ==
+        d2->file_stat.st_mtimespec.tv_nsec) {
+        return mx_strcmp(d1->name, d2->name);
+    }
+
+    return d1->file_stat.st_mtimespec.tv_sec <
+           d2->file_stat.st_mtimespec.tv_sec;
 }
