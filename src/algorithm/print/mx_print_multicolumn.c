a535b296cd3aabc86d77e4c1ee1d5fa85c7d1f9e
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-16 22:03:00 +0300

    Fix a memory leak

--- src/algorithm/print/mx_print_multicolumn.c
+++ src/algorithm/print/mx_print_multicolumn.c
@@ -3,6 +3,7 @@
 //
 
 #include <uls.h>
+#include <stdlib.h>
 
 static void reorder(t_list **list) {
     t_list *head = 0;
@@ -16,7 +17,9 @@ static void reorder(t_list **list) {
         else
             mx_push_back(&tail, work->data);
         i++;
+        t_list *node = work;
         work = work->next;
+        free(node);
     }
 
     mx_list_attach_back(&head, tail);
