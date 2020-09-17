4a759f9232f27c360c65ba899ab25bec387cf0a8
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-17 22:26:45 +0300

    Add -u

--- src/algorithm/sort/mx_time_access_sort.c
+++ src/algorithm/sort/mx_time_access_sort.c
@@ -5,6 +5,11 @@
 #include <libmx.h>
 
 int mx_time_access_sort(t_dirent *d1, t_dirent *d2) {
-    return d1->file_stat.st_atimespec.tv_nsec <
-           d2->file_stat.st_atimespec.tv_nsec;
+    if (d1->file_stat.st_atimespec.tv_sec ==
+        d2->file_stat.st_atimespec.tv_nsec) {
+        return mx_strcmp(d1->name, d2->name);
+    }
+
+    return d1->file_stat.st_atimespec.tv_sec <=
+           d2->file_stat.st_atimespec.tv_sec;
 }
