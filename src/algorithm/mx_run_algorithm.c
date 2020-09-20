0ac2d4ffb682101a89189efe494a5060fbc08cd8
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-20 21:40:12 +0300

    Try to fix multipath printing

--- src/algorithm/mx_run_algorithm.c
+++ src/algorithm/mx_run_algorithm.c
@@ -23,9 +23,8 @@ static void run_printer(t_printer printer, t_list *dirs) {
     }
     else if (printer.is_recursive == true)
         mx_print_dirs_recursive(dirs, printer.delim, printer.printer);
-    else {
+    else
         mx_print_dirs_long(dirs, printer.delim, printer.printer);
-    }
 }
 
 static void delete_dirs_list(t_list **dirs) {
@@ -33,6 +32,31 @@ static void delete_dirs_list(t_list **dirs) {
         t_dir *dir = (t_dir *)((*dirs)->data);
         mx_dirdelete(&dir);
         mx_pop_front(dirs);
+    }
+}
+
+static void reorganize_list(t_list **dirs) {
+    t_dir *dir = mx_dirnew();
+    t_list *entities = 0;
+
+    t_list *work = *dirs;
+    while (work) {
+        t_dir *current_dir = (t_dir *)work->data;
+        if (current_dir->entities) {
+            t_dirent *first_ent = (t_dirent *)current_dir->entities->data;
+            if (first_ent->file_lonely) {
+                mx_push_back(&entities, first_ent);
+                mx_pop_front(dirs);
+            }
+        }
+
+        work = work->next;
+    }
+
+    dir->entities = entities;
+    if (entities) {
+        mx_push_front(dirs, dir);
+        dir->name = mx_strdup("");
     }
 }
 
@@ -44,6 +68,9 @@ static t_list *fetch_all(t_list *paths, t_fetcher fetcher) {
         mx_list_attach_back(&result, one_dir);
         paths = paths->next;
     }
+
+    reorganize_list(&result);
+
     return result;
 }
 
