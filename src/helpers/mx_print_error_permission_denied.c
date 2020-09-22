6ed2179b9ef93749bfd6185a8f4987f83e0e675e
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2020-09-22 14:07:47 +0300

    Left only dir name for permission denied

--- src/helpers/mx_print_error_permission_denied.c
+++ src/helpers/mx_print_error_permission_denied.c
@@ -4,9 +4,21 @@
 
 #include <uls.h>
 
+char *path_last_component(char *s) {
+    int len = mx_strlen(s);
+    for (int i = len - 1; i >= 0; i--) {
+        if (s[i] == '/') {
+            char *r = &(s[i]) + 1;
+            return r;
+        }
+    }
+    return s;
+}
+
 void mx_print_error_permission_denied(char *name) {
     mx_printerr("uls: ");
-    mx_printerr(name);
+    char *last = path_last_component(name);
+    mx_printerr(last);
     mx_printerr(": Permission denied\n");
     mx_exit_code(1);
 }
